47c013245b3eb61bc9e539cfb0a90d92d347f8bf
Vendigo <119477860+Vendifo@users.noreply.github.com>
2024-03-24 03:07:20 +0900

    Бургер меню

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 $(function() {
-
-    $('.burger, .nav__list  a').on('click', function (e) {
-    e.preventDefault()
-    $('.nav__list').toggleClass('header__hight__menu--open')
-    })
+    $('.burger').on('click', function (e) {
+        e.preventDefault(); // Отменяем стандартное действие ссылки
+        $('.nav__list').toggleClass('nav__list--open'); // Переключаем класс для открытия/закрытия меню
+        $('.burger__line--one').toggleClass('open'); // Переключаем класс для первой линии бургера
+        $('.burger__line--two').toggleClass('open'); // Переключаем класс для второй линии бургера
+        $('.burger__line--three').toggleClass('open'); // Переключаем класс для третьей линии бургера
+    });
 
     $(".nav__item a").on("click", function (e) {
-        //отменяем стандартную обработку нажатия по ссылке
-        e.preventDefault()
-        //забираем идентификатор бока с атрибута href
-        var id  = $(this).attr('href'),
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top - 100
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1500)
-    })
-})
+        e.preventDefault(); // Отменяем стандартное действие ссылки
+        var id = $(this).attr('href'); // Получаем id элемента, на который ссылается якорь
+        var top = $(id).offset().top - 100; // Вычисляем положение элемента относительно верха страницы
+        $('body,html').animate({scrollTop: top}, 1500); // Плавно прокручиваем страницу к нужному элементу
+    });
+
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('.burger').length && !$(e.target).closest('.nav__list').length) {
+            // Если клик был вне бургера и меню, закрываем меню
+            $('.nav__list').removeClass('nav__list--open');
+            $('.burger__line--one').removeClass('open'); // Возвращаем первую линию в исходное состояние
+            $('.burger__line--two').removeClass('open'); // Возвращаем вторую линию в исходное состояние
+            $('.burger__line--three').removeClass('open'); // Возвращаем третью линию в исходное состояние
+        }
+    });
+});
